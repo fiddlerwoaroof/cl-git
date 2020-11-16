019acfa14c7ff09c963c327b6f73c0f165672859
Ed L <el-github@elangley.org>
2020-11-16 00:21:32 -0800

    chore: make tests more reliable

--- docker-run/main.lisp
+++ docker-run/main.lisp
@@ -1,11 +1,18 @@
-#.(progn
-    (fwoar.repl-utils:clone "https://github.com/fiddlerwoaroof/fwoar.lisputils.git" "fwoar-lisputils")
-    (fwoar.repl-utils:github "fiddlerwoaroof" "cl-git")
-    (ql:quickload :cl-git)
-    (ql:quickload :cl-git/tests)
-    nil)
+#.(handler-case (progn
+                  (fwoar.repl-utils:clone "https://github.com/fiddlerwoaroof/fwoar.lisputils.git" "fwoar-lisputils")
+                  (fwoar.repl-utils:github "fiddlerwoaroof" "cl-git")
+                  (ql:quickload :cl-git)
+                  (ql:quickload :cl-git/tests)
+                  (error "foobar")
+                  nil)
+    (serious-condition (c)
+      (format t "~&Exiting on condition: ~s~%" c)
+      (sb-ext:exit 40)))
 
-(sb-ext:exit
- :code (if (5am:explain! (5am:run :fwoar.cl-git))
-           0
-           42))
+(handler-case (sb-ext:exit
+               :code (if (5am:explain! (5am:run :fwoar.cl-gir))
+                         0
+                         42))
+  (serious-condition (c)
+    (format t "~&Exiting on condition: ~s~%" c)
+    (sb-ext:exit 43)))
