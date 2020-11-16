0ec55f5520bb0dff1dfe66ea541d04a5aa29ff3a
Ed L <el-github@elangley.org>
2020-11-16 00:32:34 -0800

    chore: making CI more reliable

--- docker-run/main.lisp
+++ docker-run/main.lisp
@@ -8,10 +8,12 @@
       (format t "~&Exiting on condition: ~s~%" c)
       (sb-ext:exit :code 40)))
 
-(handler-case (sb-ext:exit
-               :code (if (5am:explain! (5am:run :fwoar.cl-gir))
-                         0
-                         42))
+(handler-case (alexandria:if-let ((results (5am:run :fwoar.cl-gir)))
+                (sb-ext:exit
+                 :code (if (5am:explain! results)
+                           0
+                           42))
+                (sb-ext:exit :code 41))
   (serious-condition (c)
     (format t "~&Exiting on condition: ~s~%" c)
     (sb-ext:exit :code 43)))
